fa5b81a5cef01ac28645851acce878f970f91caf
Dinesh Kumar <dineshzz600@outlook.com>
2022-01-10 13:45:29 +0530

    Dines kumar

---
+++ Address.java
@@ -0,0 +1,70 @@
+public class Address{
+	private String addressLine1;
+	private String addressLine2;
+	private String addressLine3;
+	private String city;
+	private String landmark;
+	private String state;
+	private String country;
+	private String zipcode;
+	private String phone;
+	
+	  public Address(){
+	  }
+		public String getAddressLine1(){
+		return addressLine1;
+		}
+		public void setAddressLine1(String addressLine1){
+		this.addressLine1 = addressLine1;
+		}
+		public String getAddressLine2(){
+		return addressLine2;
+		}
+		public void setAddressLine(String addressLine2){
+		this.addressLine2  = addressLine2;
+		}
+		public String getAddressLine3(){
+		return addressLine3;
+		}
+		public void setAddressLine3(String addressLine3){
+		this.addressLine3 = addressLine3;
+		}
+		public String getCity(){
+		return city;
+		}
+		public void setCity(String city){
+		this.city = city;
+		}
+		public String getLandmark(){
+		return landmark;
+		}
+		public void setLandmark(String landmark){
+		this.landmark = landmark;
+		}
+		public String getState(){
+		return state;
+		}
+		public void setState(String state){
+		this.state  =state;
+		}
+		public String getCountry(){
+		return country;
+		}
+		public void setCountry( String country){
+		this.country = country;
+		}
+		public String getZipcode(){
+		return zipcode;
+		}
+		public void setZipcode(String zipcode){
+		this.zipcode = zipcode;
+		}
+		public String getPhone(){
+		return phone;
+		}
+		public void setPhone(String phone){
+		this.phone = phone;
+		}
+		
+
+}
