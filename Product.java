5d0c16e1da62d44bd1c7c558cd7793a9a9a4746d
Dinesh Kumar <dineshzz600@outlook.com>
2022-01-10 14:35:50 +0530

    Successfully Commited

---
+++ Product.java
@@ -0,0 +1,76 @@
+public class Product{
+	    private String url;
+		private String name;
+		private String model;
+		private String manufacturer;
+		private String category;
+		private String subCategory;
+		private double price;
+		private int quantity;
+		private String discription;
+		private String additionalinfo;
+		
+		public Product(){
+		}
+		public String getUrl(){
+		return url;
+		}
+		public void setUrl(String url){
+		this.url = url;
+		}
+		public String getName(){
+		return name;
+		}
+		public void setName(String name){
+		this.name = name;
+		}
+		public String getModel(){
+		return model;
+		}
+		public void setModel(String model){
+		this.model = model;
+		}
+		public String getManufacturer(){
+		return manufacturer;
+		}
+		public void setManufacturer(String manufacturer){
+		this.manufacturer = manufacturer;
+		}
+		public String getCategory(){
+		return category;
+		}
+		public void setCategory(String category ){
+		this.category = category;
+		}
+		public String getSubCategory(){
+		return subCategory;
+		}
+		public double getPrice(){
+		return price;
+		}
+		public void setPrice(double price ){
+		this.price = price;
+		}
+		public int getQuantity(){
+		return quantity;
+		}
+		public void setQuantity(int quantity){
+		this.quantity = quantity;
+		}
+		public String getDiscription(){
+		return discription;
+		}
+		public void setDiscription(String discription){
+		this.discription = discription;
+		}
+		public String getAdditionalinfo(){
+		return additionalinfo;
+		}
+		 public void setAdditionalinfo(){
+		 this.additionalinfo = additionalinfo;
+		 }
+		
+	
+
+
+}
